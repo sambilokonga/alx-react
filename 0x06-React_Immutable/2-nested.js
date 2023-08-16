a2e6a79b9875f7129cd56f0a2a582964a2c8ad91
sambilokonga <abebawbegna@gmail.com>
2023-08-16 03:06:18 -0700

    update

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,5 @@
 import { fromJS } from 'immutable';
 
-
 export default function accessImmutableObject(object, array) {
   const mappedObj = fromJS(object);
 
--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,5 @@
 import { Map } from 'immutable';
 
-
 export const map = Map({
   1: 'Liam',
   2: 'Noah',
